600e0cb8957116fc744986ad9a5bae06cb10eee6
José Valdiviesso <zmiguel.alves@gmail.com>
2022-06-12 16:30:29 +0100

    update to influxDB 2

--- PowerBox-3Phase/PowerBox-3Phase.ino
+++ PowerBox-3Phase/PowerBox-3Phase.ino
@@ -1,65 +1,62 @@
-#include <Arduino.h>
-#include <WiFi.h>
+
 #include <WiFiMulti.h>
 #include <InfluxDbClient.h>
 #include <PZEM004Tv30.h>
 #include "config.h"
 
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+#define PZEM_SERIAL Serial2
+
 WiFiMulti WiFiMulti;
-InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
 PZEM004Tv30 pzem[3] = {
-  PZEM004Tv30(&Serial2, 0x11),
-  PZEM004Tv30(&Serial2, 0x12),
-  PZEM004Tv30(&Serial2, 0x13)
+  PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x11),
+  PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x12),
+  PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x13)
 };
 
 // Data point
 Point sensor[4] = {
-  Point("PowerBox"),
+  Point("ESP32"),
   Point("PowerBox"),
   Point("PowerBox"),
   Point("PowerBox")
 };
 
+unsigned long last_time = 0;
+
 void ConnectToWiFiMulti() {
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(SSID, WiFiPassword);
-  Serial.println("Connecting Wifi...");
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void ConnectToInflux() {
   // Add constant tags - only once
-  sensor[0].addTag("Device", "ESP32");
-  sensor[1].addTag("Power_Phase", "Phase_1");
-  sensor[2].addTag("Power_Phase", "Phase_2");
-  sensor[3].addTag("Power_Phase", "Phase_3");
+  sensor[0].addTag("Device", "PowerBox");
+  sensor[1].addTag("Phase", "Cozinha");
+  sensor[2].addTag("Phase", "Loja");
+  sensor[3].addTag("Phase", "Casa");
 
   // Check server connection
-  if (client.validateConnection()) {
-    Serial.print("Connected to InfluxDB: ");
-    Serial.println(client.getServerUrl());
-  } else {
-    Serial.print("InfluxDB connection failed: ");
-    Serial.println(client.getLastErrorMessage());
+  if (!client.validateConnection()) {
+    ESP.restart();
   }
 }
 
 void setup() {
-  Serial.begin(115200);
   ConnectToWiFiMulti();
   ConnectToInflux();
 }
 
 void loop() {
+  unsigned long start_time = millis();
   sensor[0].clearFields();
   sensor[0].addField("wifi-rssi", WiFi.RSSI());
+  sensor[0].addField("acq-time", last_time);
   //read sensors
   for(int i=0;i<3;i++){
     sensor[i+1].clearFields();
@@ -67,55 +64,44 @@ void loop() {
     if(!isnan(voltage)){
         sensor[i+1].addField("voltage", voltage);
     } else {
-        Serial.println("Error reading voltage");
     }
     float current = pzem[i].current();
     if(!isnan(current)){
         sensor[i+1].addField("current", current);
     } else {
-        Serial.println("Error reading current");
     }
     float power = pzem[i].power();
     if(!isnan(power)){
         sensor[i+1].addField("power", power);
     } else {
-        Serial.println("Error reading power");
     }
     float energy = pzem[i].energy();
     if(!isnan(energy)){
         sensor[i+1].addField("energy", energy);
     } else {
-        Serial.println("Error reading energy");
     }
     float frequency = pzem[i].frequency();
     if(!isnan(frequency)){
         sensor[i+1].addField("freq", frequency);
     } else {
-        Serial.println("Error reading frequency");
     }
     float pf = pzem[i].pf();
     if(!isnan(pf)){
         sensor[i+1].addField("power-factor", pf);
     } else {
-        Serial.println("Error reading power factor");
     }
   }
-     // If no Wifi signal, try to reconnect it
-  if ((WiFi.RSSI() == 0) && (WiFiMulti.run() != WL_CONNECTED))
-  Serial.print("Writing: ");
-  Serial.println(sensor[0].toLineProtocol());
-  Serial.println(sensor[1].toLineProtocol());
-  Serial.println(sensor[2].toLineProtocol());
-  Serial.println(sensor[3].toLineProtocol());
+  // If no Wifi signal, try to reconnect it
+  if (WiFiMulti.run() != WL_CONNECTED) {
+    ESP.restart();
+  }
   // Write point
   for(int i=0;i<4;i++){
     if (!client.writePoint(sensor[i])) {
-      Serial.print(i);
-      Serial.print(": InfluxDB write failed: ");
-      Serial.println(client.getLastErrorMessage());
     }
   }
-  //Wait 5s
-  Serial.println("Wait ~5s");
-  delay(4000);
+  last_time = millis() - start_time;
+  unsigned long wait_time = 1000 - (millis() - start_time);
+  if(wait_time > 1000) wait_time = 100;
+  delay(wait_time);
 }
--- SolarMonitoring/SolarMonitoring.ino
+++ SolarMonitoring/SolarMonitoring.ino
@@ -1,99 +1,106 @@
-#include <Arduino.h>
-#include <WiFi.h>
 #include <WiFiMulti.h>
 #include <InfluxDbClient.h>
 #include <PZEM004Tv30.h>
 #include "config.h"
 
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+#define PZEM_SERIAL Serial2
+
 WiFiMulti WiFiMulti;
-InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
-PZEM004Tv30 pzem(&Serial2);
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+PZEM004Tv30 pzem[3] = {
+  PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x01),
+  PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x02),
+  PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x03)
+};
 
 // Data point
-Point sensor("Solar_1");
+Point sensor[4] = {
+  Point("ESP32"),
+  Point("Solar"),
+  Point("Solar"),
+  Point("Solar")
+};
+
+unsigned long last_time = 0;
 
 void ConnectToWiFiMulti() {
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(SSID, WiFiPassword);
-  Serial.println("Connecting Wifi...");
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void ConnectToInflux() {
   // Add constant tags - only once
-  sensor.addTag("device", "ESP32-Solar");
+  sensor[0].addTag("Device", "Solar");
+  sensor[1].addTag("Phase", "Casa");
+  sensor[2].addTag("Phase", "Cozinha");
+  sensor[3].addTag("Phase", "Loja");
 
   // Check server connection
-  if (client.validateConnection()) {
-    Serial.print("Connected to InfluxDB: ");
-    Serial.println(client.getServerUrl());
-  } else {
-    Serial.print("InfluxDB connection failed: ");
-    Serial.println(client.getLastErrorMessage());
+  if (!client.validateConnection()) {
+    ESP.restart();
   }
 }
 
 void setup() {
-  Serial.begin(115200);
   ConnectToWiFiMulti();
   ConnectToInflux();
 }
 
 void loop() {
-  sensor.clearFields();
-  sensor.addField("wifi-rssi", WiFi.RSSI());
-   float voltage = pzem.voltage();
+  unsigned long start_time = millis();
+  sensor[0].clearFields();
+  sensor[0].addField("wifi-rssi", WiFi.RSSI());
+  sensor[0].addField("acq-time", last_time);
+  //read sensors
+  for(int i=0;i<3;i++){
+    sensor[i+1].clearFields();
+    float voltage = pzem[i].voltage();
     if(!isnan(voltage)){
-        sensor.addField("voltage", voltage);
+        sensor[i+1].addField("voltage", voltage);
     } else {
-        Serial.println("Error reading voltage");
     }
-    float current = pzem.current();
+    float current = pzem[i].current();
     if(!isnan(current)){
-        sensor.addField("current", current);
+        sensor[i+1].addField("current", current);
     } else {
-        Serial.println("Error reading current");
     }
-    float power = pzem.power();
+    float power = pzem[i].power();
     if(!isnan(power)){
-        sensor.addField("power", power);
+        sensor[i+1].addField("power", power);
     } else {
-        Serial.println("Error reading power");
     }
-    float energy = pzem.energy();
+    float energy = pzem[i].energy();
     if(!isnan(energy)){
-        sensor.addField("energy", energy);
+        sensor[i+1].addField("energy", energy);
     } else {
-        Serial.println("Error reading energy");
     }
-    float frequency = pzem.frequency();
+    float frequency = pzem[i].frequency();
     if(!isnan(frequency)){
-        sensor.addField("freq", frequency);
+        sensor[i+1].addField("freq", frequency);
     } else {
-        Serial.println("Error reading frequency");
     }
-    float pf = pzem.pf();
+    float pf = pzem[i].pf();
     if(!isnan(pf)){
-        sensor.addField("power-factor", pf);
+        sensor[i+1].addField("power-factor", pf);
     } else {
-        Serial.println("Error reading power factor");
     }
-
-     // If no Wifi signal, try to reconnect it
-  if ((WiFi.RSSI() == 0) && (WiFiMulti.run() != WL_CONNECTED))
-    Serial.println("Wifi connection lost");
-  // Write point
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
   }
-  //Wait 5s
-  Serial.println("Wait ~5s");
-  delay(4000);
+  // If no Wifi signal, try to reconnect it
+  if (WiFiMulti.run() != WL_CONNECTED) {
+    ESP.restart();
+  }
+  // Write point
+  for(int i=0;i<4;i++){
+    if (!client.writePoint(sensor[i])) {
+    }
+  }
+  last_time = millis() - start_time;
+  unsigned long wait_time = 1000 - (millis() - start_time);
+  if(wait_time > 1000) wait_time = 100;
+  delay(wait_time);
 }
